cd52cd4d3f659df4ef4db67ee2d47c9649743f7c
rjpalermo@corepay.io <rjpalermo@corepay.io>
2023-03-23 10:05:30 -0400

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MjA4OQ/spectral.js?branch_slug=main&token=8f5a34a9-234b-43de-bd9e-c64a5d0cc079";
+export default { extends: ruleset };
